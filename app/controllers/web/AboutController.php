373be312d4487ec52597513f8f39be11723a88df
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2025-06-30 23:50:24 +0700

    feat: thêm AboutController và định tuyến trang giới thiệu

---
+++ app/controllers/web/AboutController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers\Web;
+
+class AboutController
+{
+    public function about()
+    {
+        echo "Đây là trang giới thiệu";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Core\Router;
 
 Router::get('/', 'HomeController@index');
 Router::get('/product', 'ProductController@product');
+Router::get('/about', 'AboutController@about');
