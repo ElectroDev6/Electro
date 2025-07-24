e13dbd7dc716096dc96386c474185db70b5040b9
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2025-07-24 15:47:44 +0700

    refactor: rm helper/view change to Core/View and fix logic

--- app/controllers/web/HomeController.php
+++ app/controllers/web/HomeController.php
@@ -2,10 +2,19 @@
 
 namespace App\Controllers\Web;
 
+use Core\View;
+
 class HomeController
 {
     public function index()
     {
-        render('home');
+        View::render('home', [
+            'products' => [
+                ['id' => 1, 'name' => 'Sản phẩm A', 'price' => 1000, 'image' => '/img/product1.webp'],
+                ['id' => 2, 'name' => 'Sản phẩm B', 'price' => 1000, 'image' => '/img/product1.webp'],
+                ['id' => 3, 'name' => 'Sản phẩm C', 'price' => 1000, 'image' => '/img/product1.webp'],
+                ['id' => 4, 'name' => 'Sản phẩm D', 'price' => 1000, 'image' => '/img/product1.webp'],
+            ]
+        ]);
     }
 }
--- app/views/web/login.php
+++
@@ -1,7 +0,0 @@
-<?php extend('layout.main'); ?>
-
-<?php section('content'); ?>
-<section>
-    <h1>Đây là trang login</h1>
-</section>
-<?php endSection(); ?>
