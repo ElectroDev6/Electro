c017d53715750679768be1c95331666c1db55681
nhatthang-hedgehog <falconf828@gmail.com>
2025-07-26 13:25:39 +0700

    Trang cart

---
+++ app/controllers/web/CartController.php
@@ -0,0 +1,10 @@
+<?php
+ namespace App\Controllers\Web;
+
+ class CartController
+ {
+     public function index()
+     {
+         render('cart');
+     }
+ }
---
+++ app/controllers/web/ThankyouController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers\Web;  
+
+
+class ThankyouController
+ {
+     public function index()
+     {
+         render('thankyou');
+     }
+ }
